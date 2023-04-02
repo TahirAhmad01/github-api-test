b4267955ce27f5911fa708af8f9fdd385d246a8a
Tahir Ahmad <tahirahmad0234@gmail.com>
2023-04-02 19:39:21 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-3xl">Github Api Test</header>
+    </div>
+  );
+}
+
+export default App;
